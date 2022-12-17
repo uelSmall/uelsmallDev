4ac060e861ce98a67457fd01de0cd58c88643bd5
uelSmall <uelsmall08@gmail.com>
2022-12-17 00:59:29 -0400

    change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,7 @@
     import "../app.css";
 </script>
 
-<button class="bg-blue-600 mb-2 mt-2 rounded-sm px-4 py-2"  onclick="TogetherJS(this); return false;">Start TogetherJS</button>
+<section>
+    <button class="bg-blue-600 mb-2 mt-2 rounded-sm px-4 py-2"  onclick="TogetherJS(this); return false;">Start TogetherJS</button>
+
+</section>
