dc81b2462418600115af4513e1b3d778a5bf6f55
uelSmall <uelsmall08@gmail.com>
2022-12-17 01:14:10 -0400

    udates: configs settings updated to reflect deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge:false,
+			split:true
+		})
 	},
 		preprocess: vitePreprocess()
 };
